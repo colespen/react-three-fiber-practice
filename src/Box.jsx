0f6f3cafa236abbe6830b0b7efd76ed3c1babd3c
colespen <spencerlewiscole@gmail.com>
2023-01-29 15:58:14 -0500

    useFrame, animate cubes wireframes

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,17 @@
+import { useRef, } from "react";
+import { useFrame } from "@react-three/fiber";
+
+
 export default function Box(props) {
+  const ref = useRef()
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 1 * delta
+    ref.current.rotation.y += 0.5 * delta
+    ref.current.position.y = Math.sin(state.clock.getElapsedTime())
+  })
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={ref} >
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
