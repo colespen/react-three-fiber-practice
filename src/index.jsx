cce6b9e247ed8c7c0fd4f16f40fc70a06814e146
colespen <spencerlewiscole@gmail.com>
2023-01-29 14:41:42 -0500

    begin props in Box components

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import './styles.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles.css';
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color={0x00ff00} wireframe />
-      </mesh>
-    </Canvas>
+    <App />
   </StrictMode>
-)
+);
