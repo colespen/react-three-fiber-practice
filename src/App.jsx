0906a769b8120fee7adf2dcf334f0712bc88e322
colespen <spencerlewiscole@gmail.com>
2023-01-30 18:48:45 -0500

    mess with matUtils and lerping position and colors for dynamic buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,26 @@ import Button from './Button';
 
 function Rig() {
   const { camera, mouse } = useThree();
+  console.log(camera);
   const vec = new Vector3();
   useFrame(() => {
     vec.set(mouse.x, mouse.y, camera.position.z);
     camera.position.lerp(vec, 0.025);
+    camera.lookAt(0, 0, 0);
   });
 }
 
 
 export default function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 8] }}>
+    <Canvas shadows camera={{ position: [0, 0, 9] }}>
       <Environment preset="forest" background blur={0.07} />
       <Center>
-      {[...Array(5).keys()].map((x) => (
-        <Button key={x} position-x={x * 2.5} />
-      ))}
+        {[...Array(5).keys()].map((x) =>
+          [...Array(3).keys()].map((y) => (
+            <Button key={x + y * 5} position={[x * 2.5, y * 2.5, 0]} />
+          ))
+        )}
       </Center>
       <Rig />
     </Canvas>
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,43 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { MathUtils, Color } from "three";
+
+const black = new Color('black');
+
 export default function Button(props) {
+  const ref = useRef();
+  const [hovered, setHovered] = useState(false);
+  const [selected, setSelected] = useState(false);
+  const colorTo = new Color(0x00ff00)
+
+    useFrame(() => {
+      ref.current.rotation.x = hovered ?
+        MathUtils.lerp(ref.current.rotation.x, -Math.PI * 2, 0.025)
+        : MathUtils.lerp(ref.current.rotation.x, 0, 0.025);
+
+      ref.current.position.z = selected ?
+        MathUtils.lerp(ref.current.rotation.z, 0, 0.025)
+        : MathUtils.lerp(ref.current.position.z, -3, 0.025);
+
+      ref.current.material.color.lerp(selected ? colorTo : black, 0.025);
+    });
+
   return (
-    <mesh {...props}>
+    <mesh {...props}
+      ref={ref}
+      onPointerDown={() => setSelected(!selected)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+
+    >
       <icosahedronGeometry />
+      <meshPhysicalMaterial
+        roughness={0}
+        metalness={0}
+        thickness={3.12}
+        ior={1.74}
+        transmission={1.0}
+      />
     </mesh>
   );
 }
