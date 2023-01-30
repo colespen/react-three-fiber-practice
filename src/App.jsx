ca3eec607816eee440e8097ac528066536d94f43
colespen <spencerlewiscole@gmail.com>
2023-01-30 15:08:50 -0500

    make annotations with Html component from drei and make state cache

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,68 @@
-import { Stats, OrbitControls, Environment } from '@react-three/drei';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Leva, useControls } from 'leva';
+import { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import {
+  useGLTF,
+  OrbitControls,
+  Environment,
+  Stats,
+  Html,
+} from '@react-three/drei';
+import { useControls } from 'leva';
+import Models from './models';
 
-function Env() {
-  const { height, radius, scale } = useControls('Ground', {
-    height: { value: 10, min: 0, max: 100, step: 1 },
-    radius: { value: 115, min: 0, max: 1000, step: 1 },
-    scale: { value: 100, min: 0, max: 1000, step: 1 },
-  });
-  return (
-    <Environment files="./img/venice_sunset_1k.hdr"
-      background
-      blur={0}
-      ground={{
-        height,
-        radius,
-        scale,
-      }}
-    />
-  );
+
+function Model({ url }) {
+  const { scene } = useGLTF(url);
+  const [cache, setCache] = useState({});
+
+  if (!cache[url]) {
+    const annotations = [];
+
+    scene.traverse((o) => {
+      if (o.userData.prop) {
+        annotations.push(
+          <Html
+            key={o.uuid}
+            position={[o.position.x, o.position.y, o.position.z]}
+            distanceFactor={0.25}
+          >
+            <div className="annotation">{o.userData.prop}</div>
+          </Html>
+        );
+      }
+    });
+
+    console.log('Caching JSX for url ' + url);
+    setCache({
+      ...cache,
+      [url]: <primitive object={scene}>{annotations}</primitive>,
+    });
+  }
+  return cache[url];
 }
 
-function Model() {
-  const {
-    x, y, z, visible, color, metalness, roughness,
-    clearcoat, clearcoatRoughness, transmission, ior, thickness
-  } = useControls('Suzanne', {
-    x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    visible: true,
-    color: { value: '#ffbc85' },
-    metalness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    roughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    clearcoat: { value: 1, min: 0, max: 1.0, step: 0.01 },
-    clearcoatRoughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    transmission: { value: 1.0, min: 0, max: 1.0, step: 0.01 },
-    ior: { value: 1.74, min: 1, max: 5, step: 0.01 },
-    thickness: { value: 3.12, min: 0, max: 5, step: 0.01 },
-  });
-
-  const { scene } = useLoader(GLTFLoader, './models/scene.glb');
-  console.log(scene);
-
-  return (
-    <primitive
-      object={scene}
-      position={[0, 0, 0]}
-      children-0-rotation={[x, y, z]}
-      children-0-visible={visible}
-      children-0-material-color={color}
-      children-0-material-metalness={metalness}
-      children-0-material-roughness={roughness}
-      children-0-material-clearcoat={clearcoat}
-      children-0-material-clearcoatRoughness={clearcoatRoughness}
-      children-0-material-transmission={transmission}
-      children-0-material-ior={ior}
-      children-0-material-thickness={thickness}
-
-    />
-  );
-}
 
 export default function App() {
-
+  const { model } = useControls({
+    model: {
+      value: 'hammer',
+      options: Object.keys(Models),
+    },
+  });
 
   return (
     <>
-      <Canvas camera={{ position: [-8, 5, 8] }} >
-        <Env />
-        <directionalLight position={[9.3, 3.0, 4.4]} intensity={4} />
-        <Model />
-        <OrbitControls target={[0, 1, 0]}
-          maxPolarAngle={Math.PI / 2}
-        />
-        <axesHelper args={[5]} />
+      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
+        <Environment files="./img/workshop_1k.hdr" background />
+        <group>
+          <Model url={Models[model]} />
+        </group>
+        <OrbitControls autoRotate />
         <Stats />
       </Canvas>
-      <Leva collapsed />
+      <span id="info">
+        The {model.replace(/([A-Z])/g, ' $1').toLowerCase()} is selected.
+      </span>
     </>
   );
 }
